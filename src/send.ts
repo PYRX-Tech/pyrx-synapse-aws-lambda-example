4e82a7e7c9e9ebcc77fe0a35d429805703611fd9
trieutruong <trieutruong.dev@gmail.com>
2026-04-30 21:45:46 +0700

    feat: add SYNAPSE_API_URL support for local testing

--- src/send.ts
+++ src/send.ts
@@ -1,6 +1,6 @@
 import type { APIGatewayProxyHandler } from 'aws-lambda';
 import { Synapse } from '@pyrx/synapse';
-const synapse = new Synapse({ apiKey: process.env.SYNAPSE_API_KEY!, workspaceId: process.env.SYNAPSE_WORKSPACE_ID! });
+const synapse = new Synapse({ baseUrl: process.env.SYNAPSE_API_URL || "https://synapse-api.pyrx.tech", apiKey: process.env.SYNAPSE_API_KEY!, workspaceId: process.env.SYNAPSE_WORKSPACE_ID! });
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const b = JSON.parse(event.body || '{}');
